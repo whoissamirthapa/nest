a515a11b7ececbda5a3da17082b270aed6e96208
Samir Thapa <thapasamirm577@gmail.com>
2023-06-27 15:17:26 +0545

    flasd-hflas-fhalsdf

--- src/crud/todo.module.ts
+++ src/crud/todo.module.ts
@@ -3,37 +3,42 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 import { CreateTodoHandler } from './handler/create-todo.handler';
-import { 
-    GetTodoCompletedHandler, 
-    GetTodoHandler, 
-    GetTodoOnlyHandler, 
-    GetTodoPaginationHandler, 
-    GetTodoProgressHandler 
+import {
+  GetTodoCompletedHandler,
+  GetTodoHandler,
+  GetTodoOnlyHandler,
+  GetTodoPaginationHandler,
+  GetTodoProgressHandler,
 } from './handler/get-todo.handler';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Todo, TodoSchema } from './crud.schema';
-import { DeleteManyTodoHandler, DeleteTodoHandler } from './handler/delete-todo.handler';
-import { EditTodoHandler, EditTodoProgressHandler } from './handler/update-todo.hanlder';
+import {
+  DeleteManyTodoHandler,
+  DeleteTodoHandler,
+} from './handler/delete-todo.handler';
+import {
+  EditTodoHandler,
+  EditTodoProgressHandler,
+} from './handler/update-todo.hanlder';
 
 @Module({
-    imports: [
-        CqrsModule,
-        MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema}])
-    ],
-    controllers: [TodoController],
-    providers: [
-        TodoService,
-        CreateTodoHandler,
-        GetTodoHandler,
-        GetTodoPaginationHandler,
-        DeleteTodoHandler,
-        EditTodoHandler,
-        EditTodoProgressHandler,
-        GetTodoOnlyHandler,
-        GetTodoProgressHandler,
-        GetTodoCompletedHandler,
-        DeleteManyTodoHandler
-    ]
+  imports: [
+    CqrsModule,
+    MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }]),
+  ],
+  controllers: [TodoController],
+  providers: [
+    TodoService,
+    CreateTodoHandler,
+    GetTodoHandler,
+    GetTodoPaginationHandler,
+    DeleteTodoHandler,
+    EditTodoHandler,
+    EditTodoProgressHandler,
+    GetTodoOnlyHandler,
+    GetTodoProgressHandler,
+    GetTodoCompletedHandler,
+    DeleteManyTodoHandler,
+  ],
 })
-
-export class TodoModule{}
+export class TodoModule {}
